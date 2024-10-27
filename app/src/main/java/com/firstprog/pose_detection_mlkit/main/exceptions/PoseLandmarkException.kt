4ba2de40f3f6bc79a554c6d553381256bbff8fbe
joaocarlosms <jcmeseverino@gmail.com>
2024-10-27 11:34:37 -0300

    fix: correcting type parameter

--- app/src/main/java/com/firstprog/pose_detection_mlkit/main/exceptions/PoseLandmarkException.kt
+++ app/src/main/java/com/firstprog/pose_detection_mlkit/main/exceptions/PoseLandmarkException.kt
@@ -1,9 +1,8 @@
 package com.firstprog.pose_detection_mlkit.main.exceptions
 
 import com.google.mlkit.vision.pose.Pose
+import com.google.mlkit.vision.pose.PoseLandmark
 
 class PoseLandmarkException(message: String): RuntimeException(message)
 
-class PoseLandMarkNullException(pose: MutableList<Pose>, message: String): IllegalStateException(
-    "$message - $pose"
-)
+class PoseLandMarkNullException(pose: MutableList<PoseLandmark>, message: String): IllegalStateException("$message - $pose")
